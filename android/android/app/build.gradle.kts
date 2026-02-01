0b881b7f5fbf054b3c761001df868d111abc24d3
Daniel Pelikan <daniel.pelikan@rohlik.cz>
2026-02-01 13:15:39 +0100

    chore: bump version to 0.4.1 for documentation cleanup release

--- android/android/app/build.gradle.kts
+++ android/android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.speech2prompt"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "com.speech2prompt.HiltTestRunner"
 
