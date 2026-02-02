2d2ee22088db1849d88e9003c9d550cf436f5b10
Daniel Pelikan <daniel.pelikan@rohlik.cz>
2026-02-02 16:50:17 +0100

    chore: bump version to 0.7.0 for release

--- android/android/app/build.gradle.kts
+++ android/android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.speech2prompt"
         minSdk = 33
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.6.0"
+        versionCode = 8
+        versionName = "0.7.0"
 
         testInstrumentationRunner = "com.speech2prompt.HiltTestRunner"
 
